897d39e5f02fe1317b55efd3b5ce0984c4861dc0
Людмила Кузнецова <ananim2023@list.ru>
2024-08-05 21:36:02 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,18 @@
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+first_name="всего задач:"
+Количество_выполненных_ДЗ=12
+second_name ="затрачено часов:"
+Количество_затраченных_часов= 1.5
+name3="среднее время выполнения"
+время_на_одно_задание=(1.5/12)
+name="курс:"
+name4="Python"
+print(name,name4,first_name,Количество_выполненных_ДЗ,second_name,Количество_затраченных_часов,name3,время_на_одно_задание)
+
+
+
+
